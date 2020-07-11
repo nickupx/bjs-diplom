35984b8c135dee4258898296cc22dd32a67bd767
nickupx <nick.upsky@gmail.com>
2020-07-11 13:29:32 +0300

    conversion, transfer (check), favorites

--- public/homePage.js
+++ public/homePage.js
@@ -22,21 +22,21 @@ ApiConnector.current((r) => {
 // Рефреш интерфейса
 
 const refresh = (result, message, func, where) => {
-    if (result.success) {
-        func(result.data);
-        where.setMessage(false, message);
-    } else {
-        where.setMessage(true, result.data);
-    }
-}
+  if (result.success) {
+    func(result.data);
+    where.setMessage(false, message);
+  } else {
+    where.setMessage(true, result.data);
+  }
+};
 
 // Получение текущих курсов валюты
 
-function getStocks() {
+const getStocks = () => {
   ApiConnector.getStocks((r) => {
     if (r.success) {
-        rates.clearTable()
-        rates.fillTable(r.data)
+      rates.clearTable();
+      rates.fillTable(r.data);
     }
   });
 }
@@ -48,17 +48,80 @@ setInterval(getStocks, 60000);
 // Операции с деньгами
 
 const money = new MoneyManager();
+
+// Пополнение
+
+money.addMoneyCallback = (data) => {
+  ApiConnector.addMoney(data, (r) => {
+    refresh(
+      r,
+      `Пополнили баланс на ${data.amount} ${data.currency}`,
+      ProfileWidget.showProfile,
+      money
+    );
+  });
+};
+
+// Конвертация
+
+money.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (r) => {
+    refresh(
+      r,
+      `Конвертировали ${data.fromAmount} ${data.fromCurrency} в ${data.targetCurrency}`,
+      ProfileWidget.showProfile,
+      money
+    );
+  });
+};
+
+// Перевод
+
+money.sendMoneyCallback = (data) => {
+  if (data.amount > 0 && data.currency) { // проверяем, чтобы не 0, и чтобы валюта была выбрана (так как api пропускает 0)
+  ApiConnector.transferMoney(data, (r) => {
+    refresh(
+      r,
+      `Перевели ${data.amount} ${data.currency} пользователю с ID ${data.to}`,
+      ProfileWidget.showProfile,
+      money
+    );
+  });
+  } else {
+    money.setMessage(true, 'Будьте внимательнее :-)')
+  }
+};
+
+// Избранное
+
 const fav = new FavoritesWidget();
 
-money.addMoneyCallback = data => {
-    ApiConnector.addMoney(data, r => {
-        refresh(r, `Пополнили баланс на ${data.amount} ${data.currency}`, ProfileWidget.showProfile, money)
-    })
-}
+const updateFav = (list) => {
+  fav.clearTable();
+  fav.fillTable(list);
+  money.updateUsersList(list);
+};
 
-//
+ApiConnector.getFavorites((r) => {
+  if (r.success) {
+    updateFav(r.data);
+  }
+});
 
+fav.addUserCallback = () => {
+  const user = fav.getData();
+  ApiConnector.addUserToFavorites(user, (r) =>
+    refresh(
+      r,
+      `Пользователь ${user.name} с ID ${user.id} добавлен в избранное`,
+      updateFav,
+      fav
+    )
+  );
+};
 
-
-
-
+fav.removeUserCallback = (id) => {
+  ApiConnector.removeUserFromFavorites(id, (r) => {
+    refresh(r, `Пользователь c ID ${id} удален из избранного`, updateFav, fav);
+  });
+};
